f03ec13f0a3591aac49c8f5f4de2b6ad44a84216
mr21cbs <mr21cbs@gmail.com>
2020-08-01 14:37:53 -0400

    initial xmptweb setup

---
+++ xmptweb/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mroytman/Documents/Anzrz/OneDrive Anzrz/OneDrive - Anzrz/Indirect Tax/Xmpt/Development/web/xmptweb/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
